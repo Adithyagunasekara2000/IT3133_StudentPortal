b6e6b354dfe34d7f66b609972dac0041f4f5df31
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2024-11-19 14:11:21 +0000

    last

--- student_information_portal/src/components/Studentable.js
+++ student_information_portal/src/components/Studentable.js
@@ -7,8 +7,20 @@ import {useState} from 'react'
 
 export default function Studentable(props) {
   const [stu,setStu]=useState(props.students[0]);
+  const [fontSize, setFontSize] = useState('medium');
+
+  const handleFontSizeChange = (size) => {
+    setFontSize(size);
+  };
   return (
+    <div className={`font-${fontSize}`}>
     <div>
+      <h5> Font Size 
+      <button onClick={() => handleFontSizeChange('small')}>S</button>
+      <button onClick={() => handleFontSizeChange('medium')}>M</button>
+      <button onClick={() => handleFontSizeChange('large')}>L</button>
+    </h5>
+    </div>
     <table border='1'>
         <tr>
             <td> First name </td>
